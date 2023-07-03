109c66623c85ded5ffe9fbe12f7a8554fefb2b01
Caio Alves <caio.alves@frogstreet.com>
2023-07-03 10:03:24 -0300

    [FEATURE] crud with express

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+const server = express();
 
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+server.use(express.json());
+
+const cursos = ['Caio em node', 'Desenvolvimento de apis', 'teste'];
+
+//return one course
+server.get('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(cursos[index]);
 });
 
-router.get('/sobre', function(req, res) {
-    res.sendFile(path.join(__dirname+'/sobre.html'));
+//return all of courses
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
 });
 
-app.use('/', router);
-app.listen(process.env.port || 3000);
+//create new one
+server.post('/cursos', (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+//update
+server.put('cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+//delete
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    cursos.splice(index, 1);
+    return res.json({ message: "Curso deletado" });
+});
+
+server.listen(process.env.port || 3000);
